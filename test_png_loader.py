f4185cbda77e7a7b464eee9bbf3ca4f1a5c730aa
logickee <logickee@gmail.com>
2018-12-31 23:43:21 +0800

    update,

--- test_png_loader.py
+++ test_png_loader.py
@@ -9,7 +9,14 @@ from pprint import pprint
 import subprocess
 
 
-input_dir='./pngs/weather/16'
+input_dirs=[
+    './pngs/weather/16',
+    './pngs/interface/16',
+    './pngs/foods/16',
+    './pngs/commerce/16',
+    './pngs/editor/16',
+]
+
 
 def testPNGLoader(input_dir):
     filenames = os.listdir(input_dir)
@@ -20,8 +27,9 @@ def testPNGLoader(input_dir):
     return filenames_with_path
 
 if __name__ == '__main__':
-    filenames_with_path = testPNGLoader(input_dir)
-    for filename in filenames_with_path:
-        command = 'python3 ./test_png.py %s' % filename
-        command_result = subprocess.check_output(command.split(' '))
-        print(command_result.decode('utf-8'))
+    for input_dir in input_dirs:
+        filenames_with_path = testPNGLoader(input_dir)
+        for filename in filenames_with_path:
+            command = 'python3 ./test_png.py %s' % filename
+            command_result = subprocess.check_output(command.split(' '))
+            print(command_result.decode('utf-8'))
